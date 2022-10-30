c88a9933dd365e8747ab506673816cf6f681aed0
Aletoria Booker <abooker77@yahoo.com>
2022-10-30 06:25:12 -0500

    code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,116 +1,75 @@
 // Create global variables
-    var currDate = $('#currentDay');
+var currTime  = moment().format('x'),
+    startHour = moment().hour(1),
+    endHour   = moment().hour(9),
+    gap       = moment.duration(endHour - startHour).hours()
+    plusOne   = startHour.add(1,'h');
 
-
-/*function convertTime(start,end) {
-
-    var output = 0,
-        convertMe = 0;
-
-    var seconds = 1000, // 1 second
-         minute = seconds * 60, // 60 seconds = 1 minute, 60,000 ms
-           hour = minute * 60, // 60 minutes = 1 hour, 3,600,000 ms
-            day = hour * 24; // 24 hours = 1 day, 86,400,000 ms
-
-    // Set time blocks to only show 8 hours 9am - 5pm
-
-    var currTime = new Date().getTime(); // convert time to ms
-    
-    var startHour = new Date().setHours(0,0,0,0); // convert time to 9am
-    //console.log(moment(startHour).format("hh a"));
-    
-    var endHour = new Date().setHours(7,0,0,0); // convert time to 5pm
-
-    var gap = endHour - startHour; // subtract end hour from start hour
-
-    var calcHour = Math.floor(gap % day / hour);
-
-    var convertMe = moment(currTime).format("h a");
-
-    for (var i=0; i < calcHour; i++) {
-        //post += startHour + i;
-        if (currTime >= startHour && currTime < endHour ) {
-            output = convertMe + [i+1]; 
-            
-        } else {
-            output = "What are you doing?!";
-        }
-
-        //console.log(post);
-        console.log(output);
+// Output time block rows
+function timeBlocks(duration) {
+    for (var i=0; i < duration; i++) {  
+        addRows();   
     }
 
-    //console.log(post);
-/*
-    if (hours >= 12) {
-        //console.log("It's AM!");
-        merīdiem = "<span>AM</span>";
-    } else {
-        //console.log("It's PM!");
-        merīdiem = "<span>PM</span>";
-    }
-    
-    hours = hours % 12;*/
-    //console.log(hours);
-    
-//}*/
-
+    return;
+}
 // Get date
 function currentDate() {
-    var currDateVal = $('#currentDay');
+    var currDate = $('#currentDay');
 
-    currDateVal.html(moment().format('MMMM DD, YYYY'));
+    currDate.text(moment().format('MMMM DD, YYYY'));
+
+    return;
 }
 
 function init() {
     currentDate();
-    timeBlocks();
-    setInterval(currentDate, 1000);
+    
+    // Pass duration to another function
+    timeBlocks(gap);
+
+    //setInterval(currentDate, 1000);
+
+    return;
 }
 
-function saveEntry() {
+function addRows() {
+    // Create variables for elements
+    var blockCont = $('.container'),
+        hrBlock   = $('<div>'),
+        taskBlock = $('<div>'),
+        saveBlock = $('<div>'),
+        blockRow  = $('<div>'),
+        newRow    = $('<div>').addClass('row');
 
-}
+    /*console.log(currTime);
+    console.log(startHour);
+    console.log(endHour);
+    console.log(gap);*/
 
-function timeBlocks() {
-    //<div class="row time-blocks">
-    var output = '';
+    // Create form elements
+    var form = $('<form>'),
+        task = '<textarea name="myTask" form="schedule-form">Enter text here...</textarea>';
 
-    var blockCont = $('.time-blocks');
+    // Format elements   
+    hrBlock.addClass('col-1 hrBlock'); 
+    taskBlock.addClass('col taskContainer');
+    form.attr('id','schedule-form').attr('method','POST');        
+    saveBlock.addClass('col-1 saveBtn').html('<button type="submit" value=""><i class="fa-solid fa-floppy-disk fa-2xl"></i></button>');
+    
+    form.append(task,saveBlock);
+    newRow.append(form);
+    taskBlock.append(newRow);  
+    blockRow.addClass('row time-blocks').append(hrBlock,taskBlock);
+    blockCont.append(blockRow);  
 
-    var hrBlock = $('<td>');
-
-    var taskBlock = $('<td>');
-
-    var saveBlock = $('<td>');
-
-    var currTime = moment().hour();
-    console.log("currtime " + currTime);
-
-    var startHour = moment().hour(1);
-    console.log("start " + startHour);
-
-    var endHour = moment().hour(9);
-    console.log("end " + endHour);
-
-    var gap = Math.ceil(moment.duration(startHour.diff(endHour))/3600000 * -1);
-    console.log("gap " + gap);
-
-    var plusHour = moment.duration(1, 'h');
-    console.log("add hour" + plusHour);
-
-    for (var i=0; i < gap; i++) {
-        //post += startHour + i;
-        if (currTime >= startHour && currTime < endHour ) {
-            output = "ok"; 
-            
-        } else {
-            output = "What are you doing?!";
-        }
-
-        console.log(output);
-    }
-
+    return;
 } 
 
 init();
+/* hrBlock.text(moment().hour(plusOne));
+if (currTime >= startHour && currTime < endHour ) {
+    hrBlock.text(plusOne);
+} else {
+    console.log("What are you doing?!");
+}*/
