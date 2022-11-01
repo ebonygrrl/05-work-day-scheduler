1c113ff388a262e9a58f780d5f90fbbd9faecc2b
Aletoria Booker <abooker77@yahoo.com>
2022-10-31 23:18:27 -0500

    code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,50 @@
 // Create global variables
+var currDate = $('#currentDay');
+var save     = document.querySelector('.saveBtn');
+
 var blockCont = $('.container'),
-    currTime  = moment().get('hour');
+    currTime  = moment().format('h A');
+    start     = moment().startOf('day');
+    end       = moment().endOf('day');
 
+function currentDate() {
+    currDate.html(moment().format('MMMM DD, YYYY'));
+}
 
-$('.hour-block').each(function() {
-    var getHourTxt = $(this).html();
-    var numHour = parseInt(getHourTxt, 10);
-    var gap = 
-    console.log(numHour + ", " + currTime);
+function currHour() {
+    var currHour = moment().hour();
+
+    $('.hour').each(function() {
+        var numHour = $(this).text();
+        var rev = moment().set('hour', numHour).format('h A');
+        
+        if (moment().isBefore(rev)) {
+            $('.description').addClass('past');
+            console.log("past");
+        } else if (moment().isAfter(rev)) {
+            $('.description').addClass('future');
+            console.log("future");
+        } else {
+            $('.description').addClass('present');
+            console.log("present");
+        }
+    });
+}
+
+function saveTask(e) {
+    e.preventDefault();
     
-    if (numHour === currTime) {
-        $('.description').addClass('present');
-        console.log("present");
-    } else if (numHour < currTime) {
-        $('.description').addClass('past');
-        console.log("past");
-    } else {
-        $('.description').addClass('future');
-        console.log("future");
+    if (e.target !== e.currentTarget) { 
+        alert("hi");
     }
-});
+}
 
+// Listen for clicks on save button
+save.addEventListener("click", saveTask);
 
+// Get today's date
+currentDate();
 
-/*setInterval(function() {
-    currHour;
-}, 1000);*/
+// Check time every half hour
+//setInterval(function() {currHour(), 10000});
+currHour();
