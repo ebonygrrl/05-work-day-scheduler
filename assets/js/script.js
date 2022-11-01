bdd8d98e47d6ee0a5cc335b4ac2379c7db7ef7f7
Aletoria Booker <abooker77@yahoo.com>
2022-11-01 11:44:59 +0530

    code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,51 @@
 // Create global variables
-var currDate = $('#currentDay');
-var save     = document.querySelector('.saveBtn');
+var currDate = $('#currentDay'),
+    save     = document.querySelector('.saveBtn');
 
-var blockCont = $('.container'),
-    currTime  = moment().format('h A');
-    start     = moment().startOf('day');
-    end       = moment().endOf('day');
-
+// Get today's date
 function currentDate() {
     currDate.html(moment().format('MMMM DD, YYYY'));
 }
 
 function currHour() {
-    var currHour = moment().format('h A');
+    var currHour = moment().hour(); // Using hour function makes variable an integer
 
     $('.hour').each(function() {
-        var numHour = $(this).text();
-        var rev = moment().set('hour', numHour).format('h A');
+        var numHour = parseInt($(this).parent().attr('id').slice(5)); // Use parseInt to make string an integer
         
         if (numHour < currHour) {
-            $(this).siblings().children('.description').addClass('past').removeClass('present future');
-            console.log("past");
+            $(this).siblings().children('.description').addClass('past').removeClass('present future');           
         } else if (numHour === currHour) {
-            $(this).siblings().children('.description').addClass('present').removeClass('past future');
-            console.log("present");
+            $(this).siblings().children('.description').addClass('present').removeClass('past future');           
         } else {
-            $(this).siblings().children('.description').addClass('future').removeClass('past present');
-            console.log("future");
+            $(this).siblings().children('.description').addClass('future').removeClass('past present');          
         }
 
-        console.log(currHour === numHour? 'yes' : 'no');
+        //console.log(currHour === numHour? 'yes' : 'no');
+        //console.log(typeof(currHour) + ", " + typeof(numHour));
     });
 }
 
 function saveTask(e) {
-    e.preventDefault();
+    var time = $(this).parent().attr('id'), 
+        tasks = $(this).siblings('.description').val();
 
+    e.preventDefault();    
     if (e.target !== e.currentTarget) { 
         alert("hi");
+
+        
+        localStorage.setItem("saveData", JSON.stringify(saveData));    
     }
 }
 
 // Listen for clicks on save button
 save.addEventListener("click", saveTask);
+
+// Clear textarea on click
+$('.description').focus(function() {
+    $(this).val('');
+ });
 
 // Get today's date
 currentDate();
